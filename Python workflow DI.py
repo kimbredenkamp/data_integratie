88c7a04ccce26ba17b1a5bc44c5a27ae174c374b
FemkeNijman <55234333+FemkeNijman@users.noreply.github.com>
2022-06-07 23:31:40 +0200

    Update Python workflow DI.py

--- Python workflow DI.py
+++ Python workflow DI.py
@@ -27,6 +27,14 @@ def main():
 
 
 def bestand_omzetten(pdf_bestanden):
+    """
+    Functie waarin alle pdf bestanden met behulp van tabula worden omgezet naar csv.
+    Hierna worden alle bestandsnamen van deze bestanden in een lijst gezet
+    die gebruikt worden door bestand_inlezen
+
+    input: pdf_bestanden - lijst van alle nodige bestanden
+    return: csv_lijst - lijst met alle csv bestandsnamen
+    """
     csv_lijst = []
     for bestand in pdf_bestanden:
         print(bestand)
@@ -40,17 +48,30 @@ def bestand_omzetten(pdf_bestanden):
 
 
 def bestand_inlezen(csv_lijst):
+        """
+    In deze functie wordt de Profile en Conditions informatie uit
+    de csv bestsanden gehaald.
+    input: csv_lijst - lijst van alle csv bestanden
+    """
+    #lists voor de profiles en conditions
     profile = []
     conditions = []
+    #loopt over de csv bestanden
     for bestand in csv_lijst:
         bestand_open = open(bestand)
         for line in bestand_open:
+             #kijkt naar het Profile onderdeel aan de hand van verjaardag
             if "Birth month" in line:
+                #wanneer dit is herkend, sla deze lijn over om
+                #alleen de belangrijke informatie mee te nemen
                 next_line = next(bestand_open).strip()
                 profile.append(next_line.split(','))
             if "Conditions" in line:
+                #Kijkt naar het conditions onderdeel
                 for i in range(5):
                     next_line = next(bestand_open).strip()
+                    #print(next_line.split(','))
+                    #gaat door totdat het andere onderdeel is gevonden
                     conditions.append(next_line.split(','))
     return profile, conditions
 
